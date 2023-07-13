39566fcbd41cd71ad988322dc9ed5c8a58da1ec8
Qiff Ya Muhammad <qiww.dev@gmail.com>
2023-07-13 10:22:02 +0700

    prepare crud beranda

---
+++ app/Http/Controllers/Admin/Pages/BerandaPageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Pages;
+
+use App\Http\Controllers\Controller;
+use App\Models\Pages\Beranda;
+use Illuminate\Http\Request;
+
+class BerandaPageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Beranda $beranda)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Beranda $beranda)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Beranda $beranda)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Beranda $beranda)
+    {
+        //
+    }
+}
---
+++ app/Models/Pages/Beranda.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Pages;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Beranda extends Model
+{
+    use HasFactory;
+
+    protected $table = 'beranda';
+}
